22a4c81de5aab9471d127278b81c5c516911cc81
DenisLabrecque <denis.g.labrecque@gmail.com>
2018-09-24 11:51:20 -0500

    fixed scroll problem
    elements that are taller than the screen will now appear on phone

--- js/scrollview.js
+++ js/scrollview.js
@@ -11,28 +11,22 @@ $(function() {
     });
   });
 });
+
 /* http://stackoverflow.com/a/488073/477958 */
 function isScrolledIntoView(elem) {
   var docViewTop = $(window).scrollTop();
   var docViewBottom = docViewTop + $(window).height();
-  var elemOffset = 0;
   
-  if(elem.data('offset') != undefined) {
-    elemOffset = elem.data('offset');
-  }
   var elemTop = $(elem).offset().top;
-  var elemBottom = elemTop + $(elem).height();
-  
-  if(elemOffset != 0) { // custom offset is updated based on scrolling direction
-    if(docViewTop - elemTop >= 0) {
-      // scrolling up from bottom
-      elemTop = $(elem).offset().top + elemOffset;
-    } else {
-      // scrolling down from top
-      elemBottom = elemTop + $(elem).height() - elemOffset
-    }
-  }
-  
+  var elemBottom = elemTop + $(elem).outerHeight(true)
+
+   // Check whether the element is taller than the window
+   if (elemTop + elemBottom >= docViewBottom + 10) {
+      // this element must be made visible
+      $(elem).removeClass('notViewed').addClass('viewed');
+   }
+
+  // Check without an offset
   if((elemBottom <= docViewBottom) && (elemTop >= docViewTop)) {
     // once an element is visible exchange the classes
     $(elem).removeClass('notViewed').addClass('viewed');
